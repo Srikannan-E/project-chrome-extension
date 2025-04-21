1e34e114a4976ec44bebfe3152bccb85d7ecb43b
Srikannan-Esakki <srikannan.esakkiappan1@freshworks.com>
2025-04-21 18:32:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -139,3 +139,10 @@ def scheduled_retrain():
 scheduler = BackgroundScheduler()
 scheduler.add_job(scheduled_retrain, 'interval', minutes=60)
 scheduler.start()
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
+
